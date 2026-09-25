6ba56b5aa2a91cac472fca110ec815b3b51eeac1
sakaguchikayo810-spec <sakaguchi.kayo810@gmail.com>
2026-09-25 17:32:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@ function createRecommendCard(rec) {
   const avatarSrc = 'images/kayo_sakaguchi.jpg';
   
   // エピソード番号とタイトルを組み合わせて表示（#数字 タイトル の形式）
- const fullTitle = rec.episodeTitle;
+const fullTitle = rec.title || rec.episodeTitle || '';
   
   return `
     <article class="recommend-card">
